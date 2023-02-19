14c08de85e9e42fc186450442fcf303a31421a1f
Jaime Ximenes <jaime.ximenes@gmail.com>
2023-02-19 10:37:33 -0300

    adicionado buscarCep api

---
+++ src/main/java/com/agenda/app/Controller/EnderecoController.java
@@ -0,0 +1,27 @@
+package com.agenda.app.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.agenda.app.dto.Endereco;
+import com.agenda.app.service.EnderecoService;
+
+@RestController
+@RequestMapping(value = "/endereco")
+public class EnderecoController {
+    
+    @GetMapping("/{cep}")
+    public ResponseEntity<Endereco> obterCep(@PathVariable String cep) {
+
+        Endereco endereco = enderecoService.buscarEnderecoPorCep(cep);
+        return endereco != null ? ResponseEntity.ok().body(endereco) : ResponseEntity.notFound().build();
+
+    }
+
+    @Autowired
+    private EnderecoService enderecoService;
+}
